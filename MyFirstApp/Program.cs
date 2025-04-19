e183ff670b6634be02a320d3f64955068f2de6b4
Enni Britta <ebesna@gmail.com>
2025-04-19 16:46:36 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// this is a comment to my code
+
+
+// app asks user to enter their name
+// app greets user by name
+
+Console.WriteLine("Enter your name"); // Output
+// string - sõne
+string userName = Console.ReadLine(); // Input
+
+Console.WriteLine($"Hello, {userName} !"); // Output
+// string interpolation
+// alt gr 7 "{", alt gr 0 "}"
+// the devil hides in the details - suured ja väiksed algustähed on olulised
